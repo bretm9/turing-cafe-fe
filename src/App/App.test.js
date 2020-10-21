1b41ea5d2f58be951a6e106395d2aa3c288b3c29
Bret Merritt <merrittbret9@gmail.com>
2020-10-21 11:36:07 -0600

    Refactor test to function with fetch

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
 import App from './App';
@@ -9,16 +9,21 @@ describe('App', () => {
     render(<App />)
   })
 
-  it('should be able to add a new reservation', () => {
+  it('should be able to add a new reservation', async () => {
     userEvent.type(screen.getByPlaceholderText('name'), 'Bret')
     userEvent.type(screen.getByPlaceholderText('date'), '12/20')
     userEvent.type(screen.getByPlaceholderText('time'), '6:01')
     userEvent.type(screen.getByPlaceholderText('number of guests'), '123456789')
     userEvent.click(screen.getByText('Make Reservation'))
 
-    expect(screen.getByText('Bret')).toBeInTheDocument();
-    expect(screen.getByText('12/20')).toBeInTheDocument();
-    expect(screen.getByText('6:01')).toBeInTheDocument();
-    expect(screen.getByText('Number of guests: 123456789')).toBeInTheDocument();
+    const name = await waitFor(() => screen.getByText('Bret'))
+    const date = await waitFor(() => screen.getByText('12/20'))
+    const time = await waitFor(() => screen.getByText('6:01'))
+    const number = await waitFor(() => screen.getByText('Number of guests: 123456789'))
+
+    expect(name).toBeInTheDocument();
+    expect(date).toBeInTheDocument();
+    expect(time).toBeInTheDocument();
+    expect(number).toBeInTheDocument();
   });
 });
