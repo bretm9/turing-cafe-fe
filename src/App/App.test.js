2b3b75f3ef14ad3ca72a145ab342babfceff5ceb
Bret Merritt <merrittbret9@gmail.com>
2020-10-21 11:07:30 -0600

    Add test to check that a reservation gets added to section when the user adds a reservatiion

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,9 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App', () => {
+  beforeEach(() => {
+    render(<App />)
+  })
+
+  it('should be able to add a new reservation', () => {
+    userEvent.type(screen.getByPlaceholderText('name'), 'Bret')
+    userEvent.type(screen.getByPlaceholderText('date'), '12/20')
+    userEvent.type(screen.getByPlaceholderText('time'), '6:01')
+    userEvent.type(screen.getByPlaceholderText('number of guests'), '123456789')
+    userEvent.click(screen.getByText('Make Reservation'))
+
+    expect(screen.getByText('Bret')).toBeInTheDocument();
+    expect(screen.getByText('12/20')).toBeInTheDocument();
+    expect(screen.getByText('6:01')).toBeInTheDocument();
+    expect(screen.getByText('Number of guests: 123456789')).toBeInTheDocument();
+  });
 });
