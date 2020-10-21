bc336d55fee3a7e87ce01d0593d9aacc45ca6d06
Bret Merritt <merrittbret9@gmail.com>
2020-10-21 10:01:19 -0600

    Add state and render

---
+++ src/Form/Form.js
@@ -0,0 +1,20 @@
+const { Component } = require("react");
+import React, { Component } from 'react';
+
+class Form extends Component {
+  constructor() {
+    super()
+    this.state = {
+      name: "",
+      date: "",
+      time: "",
+      number: 0
+    }
+  }
+
+  render() {
+
+  }
+}
+
+export default Form;
