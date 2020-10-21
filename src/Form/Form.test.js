0846046c8e2558d78d86fdb0b9da1165bdde4d94
Bret Merritt <merrittbret9@gmail.com>
2020-10-21 11:06:10 -0600

    Add test to check that input fields value reflect user input

--- src/Form/Form.test.js
+++ src/Form/Form.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Form from './Form';
+import userEvent from '@testing-library/user-event';
 
 describe('Form', () => {
   beforeEach(() => {
@@ -15,7 +16,15 @@ describe('Form', () => {
     expect(screen.getByPlaceholderText('number of guests')).toBeInTheDocument();
   })
 
-  // it('the value should reflect in the input when data is put into the form', () => {
+  it('the value should reflect in the input when data is put into the form', () => {
+    userEvent.type(screen.getByPlaceholderText('name'), 'Bret')
+    userEvent.type(screen.getByPlaceholderText('date'), '12/20')
+    userEvent.type(screen.getByPlaceholderText('time'), '6:00')
+    userEvent.type(screen.getByPlaceholderText('number of guests'), '123456789')
     
-  // })
+    expect(screen.getByPlaceholderText('name').value).toEqual('Bret');
+    expect(screen.getByPlaceholderText('date').value).toEqual('12/20');
+    expect(screen.getByPlaceholderText('time').value).toEqual('6:00');
+    expect(screen.getByPlaceholderText('number of guests').value).toEqual('123456789');
+  })
 })
