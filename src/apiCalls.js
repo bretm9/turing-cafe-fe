cb308405e366a7e86c914de535314268ec8741bd
Bret Merritt <merrittbret9@gmail.com>
2020-10-21 11:34:04 -0600

    Add postReservation

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,3 +1,15 @@
 export const getReservations = () => {
   return fetch('http://localhost:3001/api/v1/reservations')
 }
+
+export const postReservation = (reservationData) => {
+  const init = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(reservationData)
+  }
+  return fetch('http://localhost:3001/api/v1/reservations', init)
+  .then(response => response.json())
+}
